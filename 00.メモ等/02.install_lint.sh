1cb33948f9a250a3495684e9ab1f2714796cba7f
yukarimazedofu <yukarimazedofu@gmail.com>
2022-08-20 17:32:37 +0900

    Using venv

--- 00.メモ等/02.install_lint.sh
+++
@@ -1,3 +0,0 @@
-# https://qiita.com/psychoroid/items/2c2acc06c900d2c0c8cb
-pip install flake8
-pip install autopep8
--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@ sudo apt install python-is-python3
 
 sudo apt install python3-pip -y
 sudo apt install cmake -y
+sudo apt install python3.8-venv
 
-pip install numpy
-pip install matplotlib
+python -m venv venv
+source ./venv/bin/activate
+
+pip install -r requirements.txt
